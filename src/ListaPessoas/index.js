a87567e035a7904a29a8c48ac7c586a8279303dc
lucasdav <lucassouzadavanso@gmail.com>
2024-02-25 15:47:03 -0300

    inclusao useCallback

--- src/ListaPessoas/index.js
+++ src/ListaPessoas/index.js
@@ -2,11 +2,17 @@ import { useCallback, useState } from "react";
 import Pessoa from "./Pessoa";
 
 function ListaPessoas() {
-    const [pessoas, setPessoas] = useState(["Maria", "Joao"]);
+    const [pessoas, setPessoas] = useState([{
+       nome: "Maria",
+        id: 1
+    }, {
+        nome: "Joao",
+        id: 2
+    }]);
 
     // utilizar useCallback para não renderizar elemento duplicado
     const deletarPessoa = useCallback((id) => {
-        setPessoas(listaAnterior => listaAnterior.filter((pessoa, indexAnterior) => indexAnterior !== id));
+        setPessoas(listaAnterior => listaAnterior.filter(pessoa => pessoa.id !== id));
         //abaixo é passado em [] quando ela deve ser alterada
     },[]);
 
@@ -14,12 +20,15 @@ function ListaPessoas() {
         <div className="has-text-centered">
             <ul>
             {pessoas.map((pessoa, index) => (
-                <Pessoa nome={pessoa} id={index} deletar={deletarPessoa}/>
+                <Pessoa key={pessoa.id} nome={pessoa.nome} id={pessoa.id} deletar={deletarPessoa}/>
             ))}
             </ul>
             <button
                 className="button is-primary is-outlined" 
-                onClick={() => setPessoas(listaAnterior => ([...listaAnterior, "José"]))}
+                onClick={() => setPessoas(listaAnterior => ([...listaAnterior, {
+                    nome: "José",
+                    id: 3
+                }]))}
             >
                 Adicionar Pessoa
             </button>
